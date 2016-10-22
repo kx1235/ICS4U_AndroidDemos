467cfeacb0326fa60dc34fe8e09c4b32fb26f695
kenneth xu <kx123@live.ca>
2016-10-22 16:52:21 -0400

    made edittext scrollable

--- app/src/main/java/com/strobertchs/methodmadness/MainActivity.java
+++ app/src/main/java/com/strobertchs/methodmadness/MainActivity.java
@@ -10,4 +10,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void addLine() {
+        addline()
+
+    }
 }
+
