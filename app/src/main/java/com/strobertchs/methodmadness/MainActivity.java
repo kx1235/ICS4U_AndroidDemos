ea7ff16c193e8f53a978ac170506de960c1a706b
kenneth xu <kx123@live.ca>
2016-10-22 18:03:34 -0400

    able to calculate average of only 4 students

--- app/src/main/java/com/strobertchs/methodmadness/MainActivity.java
+++ app/src/main/java/com/strobertchs/methodmadness/MainActivity.java
@@ -2,6 +2,10 @@ package com.strobertchs.methodmadness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +16,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void calculateAvg(View v){
 
-    public void addLine() {
-        addline()
+        EditText student1 = (EditText) findViewById(R.id.editText);
+        EditText student2 = (EditText) findViewById(R.id.editText2);
+        EditText student3 = (EditText) findViewById(R.id.editText3);
+        EditText student4 = (EditText) findViewById(R.id.editText4);
+
+        String first= student1.getText().toString();
+        String second= student2.getText().toString();
+        String third= student3.getText().toString();
+        String fourth= student4.getText().toString();
+        float finalFirst=Integer.parseInt(first);
+        float finalSecond=Integer.parseInt(second);
+        float finalThird=Integer.parseInt(third);
+        float finalFourth=Integer.parseInt(fourth);
+
+        float avg = (finalFirst+finalSecond+finalThird+finalFourth)/4;
+
+        String finalAvg = Float.toString(avg);
+
+
+
+
+
+
+
+        TextView textout = (TextView) findViewById(R.id.textView3);
+        textout.setText(finalAvg);
+
 
     }
 }
